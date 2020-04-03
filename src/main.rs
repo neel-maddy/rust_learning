d1a66231b672553d5158e56929f5ac23400e12ad
neel-maddy <nilamadhabbarik@yahoo.com>
2020-04-03 15:05:02 +0530

    initial commit

---
+++ src/main.rs
@@ -0,0 +1,19 @@
+use std::mem;
+fn main() {
+
+    let a:u8=123;
+    println!("a={}",a);
+    let mut b:u8=0;
+    println!("b={}",b);
+    b=45;
+    println!("b={}",b);
+
+    let mut c=123456789; // 32 bit signed i32
+    println!("c={}",size={} bytes,c,mem::size_of_val(%c));
+    c=-1;
+    println!("c={} after modification",c);
+
+
+
+
+}
